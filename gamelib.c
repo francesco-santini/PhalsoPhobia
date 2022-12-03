2c21281d008f444eb45b485852f39082c472232d
Francesco Santini <31858452+francesco-santini@users.noreply.github.com>
2022-12-03 16:01:55 +0100

    Create gamelib.c

---
+++ gamelib.c
@@ -0,0 +1,8 @@
+#include "gamelib.h"
+
+// Definizioni delle funzioni in gamelib.c.
+// Piu altre funzioni di supporto.
+// Le funzioni richiamabili in main.c non devono essere static.
+// Le altre devono essere static (non visibili all'esterno).
+
+
